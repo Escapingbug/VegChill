c6b55ff948ffa314a9e0cb00d967d3558fdc3bcc
Anciety <anciety512@gmail.com>
2018-09-13 10:56:18 +0800

    add context TODO

--- vegchill/plugins/context/context_plugin.py
+++ vegchill/plugins/context/context_plugin.py
@@ -1,5 +1,17 @@
 from vegchill.extension import VegChillPlugin, VegChillInitExt, VegChillCmdExt
 
+class ContextUtilsInitExt(VegChillInitExt):
+
+    dependency = ['vegchill.plugins.arch:arch']
+
+    def __init__(self):
+        # TODO
+        pass
+
+    @classmethod
+    def name(cls):
+        return 'util'
+
 class Plugin(VegChillPlugin):
     @classmethod
     def init_ext(cls):
