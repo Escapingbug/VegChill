90a27bf81a6a6fdabcaa46c60fd8919bc9cce562
fantasy <38097898+ly-test-fuzz@users.noreply.github.com>
2018-09-15 00:42:16 +0800

    project:complete commands-shell command

--- vegchill/plugins/shell_cmd/shell_cmd_plugin.py
+++ vegchill/plugins/shell_cmd/shell_cmd_plugin.py
@@ -36,6 +36,198 @@ class LsCmdExt(VegChillCmdExt):
         import gdb
         return gdb.COMMAND_NONE
 
+class RmCmdExt(VegChillCmdExt):
+    """ls-like functionality
+    """
+    def __init__(self, *args):
+        VegChillCmdExt.__init__(self, *args)
+
+    def __call__(self, debugger, command, exe_ctx, result):
+        """lldb support"""
+        self.invoke(command, True, result=result)
+        
+    def invoke(self, argument, from_tty, result=sys.stdout):
+        """gdb support"""
+        output = ""
+        if platform.system() != 'windows':
+            output = getoutput('rm %s' % argument)
+        else:
+            output = getoutput('DEL %s' % argument)
+        if len(output) > 0:
+            six.print_(output , file = result)
+
+    def get_short_help(self):
+        return "remove files"
+
+
+    def get_long_help(self):
+        if platform.system() != 'windows':
+           return getoutput('rm --help')
+        else:
+           return getoutput('DEL /?')
+
+
+    @classmethod
+    def cmd(cls):
+        return 'rm'
+
+    @classmethod
+    def gdb_command_class(cls):
+        import gdb
+        return gdb.COMMAND_NONE
+
+class MvCmdExt(VegChillCmdExt):
+    """ls-like functionality
+    """
+    def __init__(self, *args):
+        VegChillCmdExt.__init__(self, *args)
+
+    def __call__(self, debugger, command, exe_ctx, result):
+        """lldb support"""
+        self.invoke(command, True, result=result)
+        
+    def invoke(self, argument, from_tty, result=sys.stdout):
+        """gdb support"""
+        output = ""
+        if platform.system() != 'windows':
+            output = getoutput('mv %s' % argument)
+        else:
+            output = getoutput('MOVE %s' % argument)
+        if len(output) > 0:
+            six.print_(output , file = result)
+
+    def get_short_help(self):
+        return "move src to dest"
+
+    def get_long_help(self):
+        if platform.system() != 'windows':
+            return getoutput('mv --help')
+        else:
+            return getoutput('MOVE /?')
+
+    @classmethod
+    def cmd(cls):
+        return 'mv'
+
+    @classmethod
+    def gdb_command_class(cls):
+        import gdb
+        return gdb.COMMAND_NONE
+
+class CpCmdExt(VegChillCmdExt):
+    """ls-like functionality
+    """
+    def __init__(self, *args):
+        VegChillCmdExt.__init__(self, *args)
+
+    def __call__(self, debugger, command, exe_ctx, result):
+        """lldb support"""
+        self.invoke(command, True, result=result)
+        
+    def invoke(self, argument, from_tty, result=sys.stdout):
+        """gdb support"""
+        output = ""
+        if platform.system() != 'windows':
+            output = getoutput('cp %s' % argument)
+        else:
+            output = getoutput('COPY %s' % argument)
+        if len(output) > 0:
+            six.print_(output , file = result)
+
+    def get_short_help(self):
+        return "copy src to dest"
+
+    def get_long_help(self):
+        if platform.system() != 'windows':
+           return getoutput('cp --help')
+        else:
+           return getoutput('COPY /?')
+
+    @classmethod
+    def cmd(cls):
+        return 'cp'
+
+    @classmethod
+    def gdb_command_class(cls):
+        import gdb
+        return gdb.COMMAND_NONE
+
+class RmdirCmdExt(VegChillCmdExt):
+    """ls-like functionality
+    """
+    def __init__(self, *args):
+        VegChillCmdExt.__init__(self, *args)
+
+    def __call__(self, debugger, command, exe_ctx, result):
+        """lldb support"""
+        self.invoke(command, True, result=result)
+        
+    def invoke(self, argument, from_tty, result=sys.stdout):
+        """gdb support"""
+        output = ""
+        if platform.system() != 'windows':
+            output = getoutput('rmdir %s' % argument)
+        else:
+            output = getoutput('RD %s' % argument)
+        if len(output) > 0 :
+            six.print_(output , file = result)
+
+    def get_short_help(self):
+        return 'rmdir [OPTION]... DIRECTORY...'
+
+
+    def get_long_help(self):
+        if platform.system() != 'windows':
+           return getoutput('rmdir --help')
+        else:
+           return getoutput('rd [/S] [/Q] [drive:]path') 
+
+    @classmethod
+    def cmd(cls):
+        return 'rmdir'
+
+    @classmethod
+    def gdb_command_class(cls):
+        import gdb
+        return gdb.COMMAND_NONE
+
+class MkdirCmdExt(VegChillCmdExt):
+    """ls-like functionality
+    """
+    def __init__(self, *args):
+        VegChillCmdExt.__init__(self, *args)
+
+    def __call__(self, debugger, command, exe_ctx, result):
+        """lldb support"""
+        self.invoke(command, True, result=result)
+        
+    def invoke(self, argument, from_tty, result=sys.stdout):
+        """gdb support"""
+        output = ""
+        if platform.system() != 'windows':
+            output = getoutput('mkdir %s' % argument)
+        else:
+            output = getoutput('MKDIR %s' % argument)
+        if len(output) > 0:
+            six.print_(output , file = result)
+
+    def get_short_help(self):
+        return "create directory(ies)"
+        
+    def get_long_help(self):
+        if platform.system() != 'windows':
+           return getoutput('mkdir --help')
+        else:
+           return getoutput('MKDIR /?') 
+
+    @classmethod
+    def cmd(cls):
+        return 'mkdir'
+
+    @classmethod
+    def gdb_command_class(cls):
+        import gdb
+        return gdb.COMMAND_NONE
 
 class Plugin(VegChillPlugin):
     
@@ -45,4 +237,4 @@ class Plugin(VegChillPlugin):
 
     @classmethod
     def cmd_ext(cls):
-        return LsCmdExt
+        return [LsCmdExt , RmCmdExt , RmdirCmdExt , MkdirCmdExt , MvCmdExt , CpCmdExt]
