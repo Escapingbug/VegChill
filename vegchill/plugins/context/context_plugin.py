e1b1712974dfdb8f999fa197103f1f1ae573ea65
winmin <shannanshuibe_i@163.com>
2018-09-20 17:42:14 +0800

    add is_alive()

--- vegchill/plugins/context/context_plugin.py
+++ vegchill/plugins/context/context_plugin.py
@@ -2,12 +2,22 @@
 
 
 from vegchill.extension import VegChillPlugin, VegChillInitExt, VegChillCmdExt
-from .context import *
 from vegchill.plugins.theme import *
-
-
+import gdb
+import functools
 
 # info = default_theme_plugin.EightColorInitExt()
+
+def is_alive():
+    """
+    Check if GDB is runing
+    Return : PID (Int)
+    """
+    try:
+        return gdb.selected_inferior().pid > 0
+    except Exception as e:
+        return False
+
 class ContextUtilsInitExt(VegChillInitExt):
 
     dependency = ['vegchill.plugins.arch:arch']
@@ -16,13 +26,36 @@ class ContextUtilsInitExt(VegChillInitExt):
     def __init__(self):
         self.context_register()
 
-    @staticmethod
+    def only_is_gdb_running(f):
+        """
+        Decorator wrapper to check if GDB is running.
+        """
+        def wrapper(*args, **kwargs):
+            if is_alive():
+                return f(*args, **kwargs)
+            else:
+                print(red("No debugging session active"))
+        return wrapper
+            
+
+    # @staticmethod
+    @only_is_gdb_running
     def context_register():
         """
         Display register information of current execution context
         """
-  
+
+        
         print(blue("[%s]" % "registers".center(78,'-')))
+        try:
+            gdb.execute("info registers")
+            gdb.flush()
+            
+        except Exception as e:
+            print(e)
+
+        # print(red("Happy Hacking"))
+        
 
 
     @classmethod
